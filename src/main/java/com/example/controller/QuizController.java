a6336a8e5a2e702fd346c32d2a12e3d5bb656758
Johan Yngwe <yngwe91@msn.com>
2017-03-22 17:03:48 +0100

    Bild + alias

--- src/main/java/com/example/controller/QuizController.java
+++ src/main/java/com/example/controller/QuizController.java
@@ -9,6 +9,7 @@ import com.example.domain.User;
 import com.example.repository.QuizRepository;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jboss.logging.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -30,6 +31,7 @@ public class QuizController {
     ArrayList<Question> questions;
     ArrayList<Quiz> quizzes;
     ArrayList<Answer> answers;
+    ArrayList<String> aliasList = new ArrayList<>();
 
     @Autowired
     DataSource dataSource;
@@ -47,35 +49,45 @@ public class QuizController {
         quizzes = (ArrayList<Quiz>) repository.getQuizes();
         answers = (ArrayList<Answer>) repository.getAnswers();
     }
-    @GetMapping("")
+/*    @GetMapping("")
     public ModelAndView startPage(){
         return new ModelAndView("quiz/index");
-    }
+    }*/
     @GetMapping("/2")
     public ModelAndView page2(){
         return new ModelAndView("quiz/index2");
     }
 
 
+/*
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public String messageSender(String message) throws Exception {
         return (message.toString());
+    }
+*/
+    @MessageMapping("/alias")
+    @SendTo("/topic/aliases")
+    public ArrayList<String> addingAlias(String alias) {
+        aliasList.add(alias);
+        return aliasList;
     }
 
     @MessageMapping("/connect")
     @SendTo("/topic/quiz")
     public Content quizChannel(){
         String s = "";
-        s = "{\"question\":\""+questions.get(1).getText()+"\"";
+        int index = 0;
+        s = "{\"question\":\""+questions.get(index).getText()+"\"";
         int questionCount = 1;
         for(int i=0; i<answers.size(); i++){
 
-            if(questions.get(1).getQuestionID() == answers.get(i).getQuestion_ID()){
+            if(questions.get(index).getQuestionID() == answers.get(i).getQuestion_ID()){
                 s += ", \"text"+questionCount+"\": \""+answers.get(i).getText()+"\"";
                 questionCount++;
             }
         }
+        s += ", \"img_URL\":\""+questions.get(index).getImg_URL()+"\"";
         s += "}";
         return new Content(s);
     }
@@ -87,7 +99,6 @@ public class QuizController {
         objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
         try {
             AnswerTemp answerTemp = objectMapper.readValue(message, AnswerTemp.class);
-            System.out.println(answerTemp.toString());
 
             return new Content(answerTemp.toString());
 
